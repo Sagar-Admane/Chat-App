80e7b71d26ea9d218068d4d6e8fb63bc4514bbb2
Sagar Admane <sagaradmane6@gmail.com>
2025-03-17 14:11:22 +0530

    Changed App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,28 @@
 import React, { useState } from 'react'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import ChatPage from './Components/CHat/chatPage'
 import Login from './Components/Login/Login'
 import Signup from './Components/SignUp/Signup'
 import chatContext from './Context/chatProvider'
 import "./App.css"
 
-
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element : <Login />
-  },
-  {
-    path : "/chat",
-    element : <ChatPage />
-  }, {
-    path : "/signup",
-    element : <Signup />
-  }
-])
-
 function App() {
   const [user, setUser] = useState();
   const [open1, setOpen1] = useState(false);
   const [selectedChat, setSelectedchat] = useState();
   const [groupModal, setGroupModal] = useState(false);
-  const [chats, setChats] = useState([]);;
+  const [chats, setChats] = useState([]);
   const [notification, setNotification] = useState([]); 
   
   return (
     <chatContext.Provider value={{user, setUser, open1, setOpen1, selectedChat, setSelectedchat, chats, setChats, groupModal, setGroupModal, notification, setNotification}} >
-    <div>
-      <RouterProvider router={router} />
-    </div>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </HashRouter>
     </chatContext.Provider>
   )
 }
