273053d1eea86dea5b9571bb04054c6d9b89a22b
Sagar Admane <sagaradmane6@gmail.com>
2025-03-17 13:27:54 +0530

    changed the cors origin url

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const server = app.listen(PORT,()=>{
 const io = new Server(server,{
     pingTimeout : 60000,
     cors :{
-        origin : "http://localhost:5173",
+        origin : "https://chat-app-omega-sandy.vercel.app/",
     }
 })
 
